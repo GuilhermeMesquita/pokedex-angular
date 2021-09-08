b80bd937cf25cdb0c2ace6f225fc503e21862cac
Guilherme Rocha <guilhermemesquitarch@gmail.com>
2021-09-08 14:41:46 -0300

    added directives

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from '../../_model/Pokemon'
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent {
 
-  constructor() { }
+  @Input()
+  public pokemon: Pokemon;
 
-  ngOnInit(): void {
+
+  public leadingZero(str: string | number, size: number = 3): string {
+    let s = String(str);
+    while (s.length < (size || 2)) { s = "0" + s; }
+    return s;
   }
-
 }
+
